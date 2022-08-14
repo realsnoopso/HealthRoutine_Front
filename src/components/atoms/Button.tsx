e324084df620e9cc4439ca9d78e053c58f12e94e
Snoop So <real@snoop.so>
2022-08-14 23:28:51 +0900

    Add play button to 'start' page

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -2,19 +2,46 @@ import styled from '@emotion/styled';
 import theme from '@src/styles/theme';
 
 interface Button extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  children: React.ReactNode;
+  children?: React.ReactNode;
   _onClick: () => void;
+  shape?: 'round' | 'square';
+  icon?: string;
 }
 
 export function Button(props: Button) {
-  const { children, _onClick } = props;
-  return <StyledRoot onClick={_onClick}>{children}</StyledRoot>;
+  const { children, _onClick, shape, icon } = props;
+
+  return (
+    <StyledRoot
+      className={`${shape === 'round' ? 'round' : 'squre'}`}
+      onClick={_onClick}
+    >
+      <span className="material-icons">{icon}</span>
+      {children}
+    </StyledRoot>
+  );
 }
 
 const StyledRoot = styled.button`
-  padding: 24px;
-  min-width: 160px;
   background-color: ${theme.colors.primary};
   color: ${theme.colors.text};
-  border-radius: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  &.round {
+    padding: 16px;
+    min-width: inherit;
+    border-radius: 50%;
+  }
+
+  &.squre {
+    padding: 24px;
+    border-radius: 20px;
+    min-width: 160px;
+  }
+
+  .material-icons {
+    font-size: 56px;
+  }
 `;
--- src/pages/start.tsx
+++ src/pages/start.tsx
@@ -1,14 +1,21 @@
 import type { NextPage } from 'next';
 import { css } from '@emotion/css';
+import { Button } from '@src/components/atoms/Button';
+import { useRouter } from 'next/router';
 
 const Start: NextPage = () => {
+  const router = useRouter();
   return (
     <div className={style}>
       <h3>힙 아브덕션 이너타이</h3>
       <h1>시작</h1>
-      <div className="play">
-        <span className="material-icons">play_arrow</span>
-      </div>
+      <Button
+        shape="round"
+        icon="play_arrow"
+        _onClick={() => {
+          router.push('/doing');
+        }}
+      />
     </div>
   );
 };
@@ -21,4 +28,9 @@ const style = css`
   justify-content: center;
   align-items: center;
   height: 100vh;
+
+  Button {
+    position: absolute;
+    bottom: 56px;
+  }
 `;
